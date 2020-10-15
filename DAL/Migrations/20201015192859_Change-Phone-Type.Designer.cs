2a8fdc31ca47a984f318221e2c734d7bc798423c
Taras Vavrykovych <vavrycovych@gmail.com>
2020-10-15 22:29:58 +0300

    Add migration

--- DAL/Migrations/20201015192859_Change-Phone-Type.Designer.cs
+++ DAL/Migrations/20201015192859_Change-Phone-Type.Designer.cs
@@ -4,14 +4,16 @@ using DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(FundsContext))]
-    partial class FundsContextModelSnapshot : ModelSnapshot
+    [Migration("20201015192859_Change-Phone-Type")]
+    partial class ChangePhoneType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
