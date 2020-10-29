b74869bef328b3002069612b9b53c6a7d2489e77
Олег Заріцький <Oleh.Zaritskyy@lnu.edu.ua>
2020-10-29 23:28:51 +0200

    Create interface CurrencyService

---
+++ BLL/Interfaces/ICurrencyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Interfaces
+{
+    public interface  ICurrencyService
+    {
+        void UpdateCurrency();
+        Decimal GetRate(string code);
+        Decimal GetRateByDate(string code, DateTime date);
+    }
+}
