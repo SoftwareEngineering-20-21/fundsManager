965ddbef1a803408143f0e85e256c682d2293075
Artur Troian <troian.art@gmail.com>
2020-10-30 10:58:58 +0200

    Initial commit

---
+++ BLL/Services/StatisticsService.cs
@@ -0,0 +1,46 @@
+﻿using BLL.Interfaces;
+using BLL.Models;
+using DAL.Domain;
+using DAL.Enums;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BLL.Services
+{
+    class StatisticsService : IStatisticsService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public User CurrentUser { get; set; }
+        public StatisticsService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<StatisticsItem> GetStatistics(DateTime fromDate, DateTime toDate)
+        {
+           
+            List<StatisticsItem> Items = new List<StatisticsItem>();
+            if (CurrentUser is null)
+            {
+                throw new ArgumentException("User is null");
+            }
+
+            if (acctype == null)
+            {
+                var accounts=unitOfWork.Repository<BankAccount>().Get(x => x.Users.Select(a => a.UserId).Contains(CurrentUser.Id));
+                var transaction= unitOfWork.Repository<Transaction>().Get(x => x.Users.Select(a => a.UserId).Contains(CurrentUser.Id));
+          
+            }
+            
+      
+        }
+        public IEnumerable<StatisticsItem> GetIncomeStatistics(DateTime fromDate, DateTime toDate, AccountType? acctype = null)
+
+
+
+    }
+}
