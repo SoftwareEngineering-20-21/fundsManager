a5d6f20a6aaad5fa26e71fbb6dbc010b4620bc1a
nazarkychma <kychmanazar@gmail.com>
2020-11-26 21:51:11 +0200

    Test template

--- fundsManager/UnitTests/TestUserService.cs
+++ fundsManager/UnitTests/TestUserService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using BLL.Services;
 using DAL.Domain;
 using DAL.Interfaces;
 using Moq;
@@ -8,17 +9,37 @@ namespace UnitTests
 {
     public class TestUserService
     {
+        const string password = "fancy";
+        const string passwordHash = "$2a$04$mHManSTXI9OrvFGtT3Nsz.eyyGumdhnj/oRAGINntWjZ7D/rtupx2";
+        User testUser = new User
+        {
+            Name = "test",
+            Surname = "test",
+            Mail = "test@test.com",
+            Phone = "0999999999",
+            Password = passwordHash,
+            BankAccounts = new List<UserBankAccount>()
+        };
         [SetUp]
         public void Setup()
         {
             var mock = new Mock<IRepository<User>>();
-            mock.Setup(a => a.Get()).Returns(new List<User>());
+            mock.Setup(a => a.Get()).Returns(new List<User> { testUser });
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(uow => uow.Repository<User>()).Returns(mock.Object);
+            var service = new UserService(mockUnitOfWork.Object);
         }
 
         [Test]
-        public void Test1()
+        public void TestLogin()
         {
-            Assert.Pass();
+            var mock = new Mock<IRepository<User>>();
+            mock.Setup(a => a.Get()).Returns(new List<User> { testUser });
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(uow => uow.Repository<User>()).Returns(mock.Object);
+            var service = new UserService(mockUnitOfWork.Object);
+            User testLogin = service.Login("test@test.com", password);
+            Assert.AreEqual(testLogin.Name, "test");
         }
     }
 }
