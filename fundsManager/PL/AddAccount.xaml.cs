7d03e618d1e72eff08cdc5fbb039f86bb8da5d08
nazarkychma <kychmanazar@gmail.com>
2020-12-03 23:25:43 +0200

    Add items source to currency comobox

--- fundsManager/PL/AddAccount.xaml.cs
+++ fundsManager/PL/AddAccount.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Linq;
+using DAL.Domain;
+using DAL.Interfaces;
+using DAL.Enums;
 
 namespace PL
 {
@@ -23,6 +27,7 @@ namespace PL
         {
             InitializeComponent();
             this.kernel = kernel;
+            AddAccountCurrencyComboBox.ItemsSource = kernel.Get<IUnitOfWork>().Repository<Currency>().Get().Select(x => x.Code).ToList<String>();
         }
 
         private void AddAccountCancelButton_Click(object sender, RoutedEventArgs e)
